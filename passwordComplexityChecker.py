ee75376bf33db1f5af6e60ba8fc3e45bf958b956
jayshar1234 <jays52458@gmail.com>
2026-05-11 22:35:34 +0530

    implement password complexity checker

---
+++ passwordComplexityChecker.py
@@ -0,0 +1,20 @@
+import re
+
+password = input("Enter Password: ")
+strength = 0
+if len(password) >= 8:
+    strength += 1
+if re.search("[A-Z]", password):
+    strength += 1
+if re.search("[a-z]", password):
+    strength += 1
+if re.search("[0-9]", password):
+    strength += 1
+if re.search("[@#$%^&*!]", password):
+    strength += 1
+if strength == 5:
+    print("Strong Password")
+elif strength >= 3:
+    print("Medium Password")
+else:
+    print("Weak Password")
